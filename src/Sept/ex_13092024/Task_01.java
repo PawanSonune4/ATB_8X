0bfb4438af2467e2c60c2882e087e90a6280b0c5
PawanSonune4 <pawansonune25@gmail.com>
2024-09-20 08:15:15 +0530

    first commit

---
+++ src/Sept/ex_13092024/Task_01.java
@@ -0,0 +1,29 @@
+package Sept.ex_13092024;
+
+public class Task_01 {
+    public static void main(String[] args) {
+
+        int num= 10;
+
+        System.out.printf("%d * %d = %d",num,1,num*1); // 10*1= 10
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,2,num*2); // 10*2= 20
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,3,num*3); // 10*3= 30
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,4,num*4); // 10*4= 40
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,5,num*5); // 10*5= 50
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,6,num*6); // 10*6= 60
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,7,num*7); // 10*7= 70
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,8,num*8); // 10*8= 80
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,9,num*9); // 10*9= 90
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,10,num*10); // 10*10= 100
+        System.out.println();
+    }
+}
---
+++ src/Sept/ex_13092024/Task_02.java
@@ -0,0 +1,15 @@
+package Sept.ex_13092024;
+
+public class Task_02 {
+    public static void main(String[] args) {
+
+        double a= 34;
+        double b= 10;
+
+        System.out.println(a+b); // 34+10= 44.0
+        System.out.println(a-b); // 34-10= 24.0
+        System.out.println(a*b); // 34*10= 340.0
+        System.out.println(a/b); // 34/10= 3.4
+        System.out.println(a%b); // 34%10= 4.0
+    }
+}
